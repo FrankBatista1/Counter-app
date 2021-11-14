0ff5f4eb1a7e531c7f3e4fc93f06b63554f6e1a3
FrankBatista1 <fb53901@gmail.com>
2021-11-13 22:51:50 -0500

    react native basic instalation

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+export const App = () => {
+  return (
+    <View>
+      <Text>Basic react native configuration</Text>
+    </View>
+  )
+}
+
+export default App;
+
