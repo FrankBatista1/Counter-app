2ddcf4a97aaa390d8cf3289d5d70790b954e0b9d
FrankBatista1 <fb53901@gmail.com>
2021-11-16 23:04:23 -0500

    Button counter

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -1,14 +1,51 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import {useState} from 'react';
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
 
 const ContadorScreen = () => {
-  return (
-    <View style={{flex: 1, backgroundColor: 'red', justifyContent: 'center'}}>
-      <Text style={{textAlign: "center", fontSize: 40, position: 'relative', top: -100}}>
-contador
-      </Text>
-    </View>
-  )
-}
+  const [counter, setCounter] = useState(10);
 
-export default ContadorScreen
+  return (
+    <View style={style.container}>
+      <Text style={style.title}>counter: {counter}</Text>
+      <TouchableOpacity style={style.fabLocationBR} onPress={() => setCounter(counter + 1)}>
+        <View style={style.fab}>
+          <Text style={style.fabText}>+1</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default ContadorScreen;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  fabLocationBR: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 40,
+    position: 'relative',
+    top: -100,
+  },
+  fab: {
+  backgroundColor: '#5856D6', 
+  borderRadius: 100,
+  width: 60,
+  height: 60,
+  justifyContent: 'center'
+},
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center'
+  }
+});
